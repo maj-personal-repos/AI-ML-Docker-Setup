149fef1ee5c1c97e46b12b20c10e2421f5ada751
Miguel Alonso Jr <drmiguelalonsojr@gmail.com>
2018-08-23 09:08:30 -0400

    Potential bug fix for run_docker.bat

--- run_docker.bat
+++ run_docker.bat
@@ -1,1 +1,4 @@
-docker run --platform linux -it -p 6006:6006 -p 8888:8888 -v %~dp0::/home/user/code ai-ml bash
+SET code_dir=%cd%/code
+
+
+docker run --platform linux -it -p 6006:6006 -p 8888:8888 -v %code_dir%:/home/user/code ai-ml bash
